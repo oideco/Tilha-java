93675677b5e3e09d01b78a851179d2af8544b7f0
-- <deco.vladek@hotmail.com>
2024-05-04 20:29:46 -0300

    Operadores

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,40 @@
-public class TiposVariaveis {
+ public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+
+        // Tipos Primitivos
+
+        String meuNome = "Alessandro Xavier";
+
+        double salarioMinino = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14; //Com a palavra Reserva "final" o valor da variável não poderá ser alterada
+       
+  
+
+
+
+
+
+
+
+
     }
+
+
+
+
+
+
+
+
 }
